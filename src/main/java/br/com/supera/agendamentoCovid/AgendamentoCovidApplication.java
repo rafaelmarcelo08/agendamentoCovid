e82db58c4d891f4a70b15ed5ebfe29955e6d3b60
Rafael Marcelo <rafael.2018.ads@gmail.com>
2021-09-21 21:04:34 -0300

    Projeto iniciado

---
+++ src/main/java/br/com/supera/agendamentoCovid/AgendamentoCovidApplication.java
@@ -0,0 +1,13 @@
+package br.com.supera.agendamentoCovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoCovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoCovidApplication.class, args);
+	}
+
+}
